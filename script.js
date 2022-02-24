c20e689b1e2aa73c2a2dfa2a80dbcaaa00e11703
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-02-24 13:37:03 +0100

    create functions to check for a winner

--- script.js
+++ script.js
@@ -3,6 +3,18 @@ const board = document.querySelector("[data-board]");
 
 let isCircleTurn;
 
+// winning combinations
+let winnigCombinations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
 //Game start with "X"
 const startGame = () => {
     for(const cell of cellElements) {
@@ -12,6 +24,15 @@ const startGame = () => {
     isCircleTurn = false;
     board.classList.add("x");
 }
+
+// check the winner
+const checkForWin = (currentPlayer) => {
+    return winnigCombinations.some((combinantion) => {
+        return combinantion.every((index) => {
+            return cellElements[index].classList.contains(currentPlayer);
+        });
+    });
+};
 
 const placeMark = (cell, classToAdd) => {
     //add "X" or "cricle"
@@ -38,6 +59,12 @@ const handleClick = (e) => {
     const classToAdd = isCircleTurn ? "circle" : "x";
 
     placeMark(cell, classToAdd);
+
+    // check for the winner
+    const isWin = checkForWin(classToAdd);
+    if(isWin){
+        console.log("winner");
+    }
 
     //change symbol
     swapTurns();
