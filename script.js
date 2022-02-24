e1260f9cc4e46b860c33d1ced3889de2851f15d6
Tamiris Cristine <44439100+TamirisCss@users.noreply.github.com>
2022-02-24 13:47:54 +0100

    check the winner

--- script.js
+++ script.js
@@ -1,75 +1,84 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winningMsgDiv = document.querySelector("[data-winning-msg]");
+const winnerMsgText = document.querySelector("[data-winner-msg]");
 
 let isCircleTurn;
 
 // winning combinations
 let winnigCombinations = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 //Game start with "X"
 const startGame = () => {
-    for(const cell of cellElements) {
-        cell.addEventListener("click", handleClick, { once: true });
-    }
+  for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true });
+  }
 
-    isCircleTurn = false;
-    board.classList.add("x");
-}
+  isCircleTurn = false;
+  board.classList.add("x");
+};
+
+//winner message
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winnerMsgText.innerText = "Empate";
+  } else {
+    winnerMsgText.innerHTML = isCircleTurn ? "Circulo venceu!" : "X venceu";
+  }
+};
 
 // check the winner
 const checkForWin = (currentPlayer) => {
-    return winnigCombinations.some((combinantion) => {
-        return combinantion.every((index) => {
-            return cellElements[index].classList.contains(currentPlayer);
-        });
+  return winnigCombinations.some((combinantion) => {
+    return combinantion.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
     });
+  });
 };
 
 const placeMark = (cell, classToAdd) => {
-    //add "X" or "cricle"
-    cell.classList.add(classToAdd);
-}
+  //add "X" or "cricle"
+  cell.classList.add(classToAdd);
+};
 
 const swapTurns = () => {
-    //change symbol
-    isCircleTurn = !isCircleTurn
+  //change symbol
+  isCircleTurn = !isCircleTurn;
 
-    board.classList.remove("circle");
-    board.classList.remove("x");
+  board.classList.remove("circle");
+  board.classList.remove("x");
 
-    if(isCircleTurn) {
-        board.classList.add("circle");
-    }else {
-        board.classList.add("x");
-    }
-}
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
 
 const handleClick = (e) => {
-    // add "X" or "cricle"
-    const cell = e.target;
-    const classToAdd = isCircleTurn ? "circle" : "x";
+  // add "X" or "cricle"
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
 
-    placeMark(cell, classToAdd);
+  placeMark(cell, classToAdd);
 
-    // check for the winner
-    const isWin = checkForWin(classToAdd);
-    if(isWin){
-        console.log("winner");
-    }
+  // check for the winner
+  const isWin = checkForWin(classToAdd);
+  if (isWin) {
+    endGame(true);
+  }
 
-    //change symbol
-    swapTurns();
-}
+  //change symbol
+  swapTurns();
+};
 
 startGame();
-
-
